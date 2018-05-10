04eae46ea3ab3140a6ec224aacec419c83247083
smithe25 <36971638+smithe25@users.noreply.github.com>
2018-05-09 18:17:24 -0700

    Add files via upload

--- Lab4/ALU.vhd
+++ Lab4/ALU.vhd
@@ -33,27 +33,41 @@ architecture ALU_Arch of ALU is
 			shamt:	in std_logic_vector(4 downto 0);
 			dataout: out std_logic_vector(31 downto 0));
 	end component shift_register;
-Signal addResult, shiftResult, andResult, orResult : std_logic_vector(31 downto 0);
+
+Signal addResult, shiftData, shiftResult, andResult, orResult : std_logic_vector(31 downto 0);
 Signal bitComparison : std_logic_vector(1 downto 0);
+Signal carryOut : std_logic;
 begin
 with ALUCtrl select
-	ALUResult <= 	addResult when "00000",
-			shiftResult when "00001",
-			orResult when "00100",
-			andResult when "01000",
+shiftData <= DataIn1 when "01XXX",
+	     DataIn2 when others;
+
+with ALUCtrl select
+	ALUResult <= 	addResult when "0000X",
+			andResult when "00010",
+			orResult when "00011",
+		       shiftResult when "X1XXX",
+			DataIn2 when "10000",
 			"ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ" when others;
 
-for i in 31 downto 0 loop
-bitComparison <= DataIn1(i) & DataIn2(i);
-with bitComparison select
-	andResult(i) <= '1' when "11",
-		        '0' when others;
 
-with bitComparison select
-	orResult(i) <= '1' when "01",
-		       '1' when "10",
-		       '0' when others;
-end loop;
+
+Adder : adder_subtracter port map (DataIn1, DataIn2, ALUCtrl(0), addResult, carryOut);
+Shift : shift_register port map (shiftData, ALUCtrl(2), ALUCtrl, shiftResult);
+andResult <= DataIn1 AND DataIn2;
+orResult <= DataIn1 AND DataIn2;
+
+-- for i in 31 downto 0 loop
+--bitComparison <= DataIn1(i) & DataIn2(i);
+--with bitComparison select
+	--andResult(i) <= '1' when "11",
+		--        '0' when others;
+
+--with bitComparison select
+	--orResult(i) <= '1' when "01",
+		--       '1' when "10",
+		  --     '0' when others;
+--end loop;
 			
 	-- Add ALU VHDL implementation here
 
